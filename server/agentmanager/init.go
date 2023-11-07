a7cf9659ec6e657e57faf52243bbba190ce00608
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-11-07 16:38:38 +0800

    Modifying language errors

--- server/agentmanager/init.go
+++ server/agentmanager/init.go
@@ -9,7 +9,7 @@ const Version = "1.0.1"
 var PluginInfo = &client.PluginInfo{
 	Name:        "topology",
 	Version:     Version,
-	Description: "system application architecture perception",
+	Description: "System application architecture detection.",
 	Author:      "wangjunqi",
 	Email:       "wangjunqi@kylinos.cn",
 	Url:         "http://10.1.10.131:9991/plugin/topology",
