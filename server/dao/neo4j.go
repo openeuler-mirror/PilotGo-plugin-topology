55eacb57670d4952447d308df3c928836bcf5166
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-11-21 09:11:09 +0800

    Change single quotes to back quotes in cql

--- server/dao/neo4j.go
+++ server/dao/neo4j.go
@@ -53,7 +53,7 @@ func (n *Neo4jclient) Node_create(unixtime string, node *meta.Node) error {
 	var cqlIN string
 
 	if len(node.Metrics) == 0 {
-		cqlIN = fmt.Sprintf("create (node:`%s` {unixtime:'%s', nid:'%s', name:'%s'} set node:'%s')",
+		cqlIN = fmt.Sprintf("create (node:`%s` {unixtime:'%s', nid:'%s', name:'%s'} set node:`%s`)",
 			node.Type, unixtime, node.ID, node.Name, node.UUID)
 	} else {
 		cqlIN = fmt.Sprintf("create (node:`%s` {unixtime:'%s', nid:'%s', name:'%s'}) set node:`%s`, node += $metrics",
