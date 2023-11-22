99d52d9393744f8a23ed937b459b08dba8ac9eba
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-11-22 15:39:49 +0800

    modify graph layout in clusterview

--- web/src/views/ClusterView.vue
+++ web/src/views/ClusterView.vue
@@ -70,7 +70,7 @@ import { ref, reactive, onMounted } from "vue";
 import { topo } from '../request/api';
 import server_logo from "@/assets/icon/server.png";
 import { More, Platform, Files, Collection } from '@element-plus/icons-vue';
-import topodata from '@/assets/cluster_tags.json'
+import topodata from '@/assets/cluster-2.json'
 import { useLayoutStore } from '@/stores/charts';
 import MyEcharts from '@/views/MyEcharts.vue';
 import { pickerOptions } from '@/utils/datePicker';
@@ -110,7 +110,29 @@ const tags_color: string[] = [
   'rgb(141, 204, 147)',
   'rgb(217, 200, 174)',
   'rgb(241, 102, 103)'
-]
+];
+
+const subjectColors = [
+  '#5F95FF', // blue
+  '#61DDAA',
+  '#65789B',
+  '#F6BD16',
+  '#7262FD',
+  '#78D3F8',
+  '#9661BC',
+  '#F6903D',
+  '#008685',
+  '#F08BB4',
+];
+const backColor = '#fff';
+const theme = 'default';
+const disableColor = '#777';
+const colorSets = G6.Util.getColorSetsBySubjectColors(
+  subjectColors,
+  backColor,
+  theme,
+  disableColor,
+);
 
 function handleClose() {
   chart_drawer.value = false
@@ -130,27 +152,38 @@ onMounted(async () => {
           stroke: "red",
           lineWidth: 2,
         }
-      } else if (edge.Type === "server") {
-
+      } else {
+        edge.style = {
+          lineWidth: 3,
+        }
       }
     };
 
     for (let i = 0; i < data.data.nodes.length; i++) {
       let node: any = data.data.nodes[i];
       node.nodeStrength = -30;
-      if (node.type === "host") {
+      if (node.Type === "host") {
         node.img = server_logo;
         node.type = "image";
         node.size = 40;
         node.nodeStrength = -200;
         let ip = node.id.split("_").pop()
         node.label = ip;
-      } else if (node.type === "process") {
+      } else if (node.Type === "process") {
         node.label = node.name + ":" + node.metrics.Pid;
-      } else if (node.type === "net") {
+      } else if (node.Type === "net") {
         node.label = node.name;
       }
     };
+
+    data.data.combos.forEach((combo: any, i: any) => {
+    const color = colorSets[i % colorSets.length];
+    combo.style = {
+      stroke: color.mainStroke,
+      fill: color.mainFill,
+      opacity: 0.8
+    }
+    })
 
     initGraph(data.data);
   } catch (error) {
@@ -163,19 +196,133 @@ function initGraph(data: any) {
     container: "topo-container",
     width: document.getElementById("topo-container")!.clientWidth,
     height: document.getElementById("topo-container")!.clientHeight,
+    fitView: true,
+    fitViewPadding: 50,
+    animate: true,
+    minZoom: 0.00000001,
     layout: {
-      // type: 'force',
-      // preventOverlap: true,
-      // linkDistance: 100,
+      pipes: [
+        // {
+        //   type: 'comboCombined',
+        //   spacing: 1,
+        //   comboPadding: 10,
+        //   preventComboOverlap: true,
+        //   innerLayout: new G6.Layout['radial']({
+        //     focusNode: '54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864_host_10.10.10.20',
+        //     unitRadius: 50,
+        //     maxIteration: 500,
+        //     linkDistance: 10,
+        //     preventOverlap: true,
+        //     nodeSize: 30,
+        //     sortBy: 'layoutattr',
+        //     sortStrength: 5,
+        //     nodesFilter: (node: any) => node.uuid === '54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864',
+        //   })
+        // },
+        // {
+        //   type: 'comboCombined',
+        //   spacing: 1,
+        //   comboPadding: 20,
+        //   preventComboOverlap: true,
+        //   innerLayout: new G6.Layout['radial']({
+        //     focusNode: '070cb0b4-c415-4b6a-843b-efc51cff6b76_host_10.10.10.60',
+        //     unitRadius: 50,
+        //     maxIteration: 500,
+        //     linkDistance: 100,
+        //     preventOverlap: true,
+        //     nodeSize: 30,
+        //     sortBy: 'layoutattr',
+        //     sortStrength: 50,
+        //     nodesFilter: (node: any) => node.uuid === '070cb0b4-c415-4b6a-843b-efc51cff6b76',
+        //   })
+        // },
+        // {
+        //     type: 'comboCombined',
+        //     spacing: 1,
+        //     comboPadding: 20,
+        //     preventComboOverlap: true,
+        //     innerLayout: new G6.Layout['radial']({
+        //       focusNode: '7d0740a7-5ee6-41a9-846b-d52890d690d5_host_10.10.10.111',
+        //       unitRadius: 150,
+        //       maxIteration: 500,
+        //       linkDistance: 100,
+        //       preventOverlap: true,
+        //       nodeSize: 30,
+        //       sortBy: 'layoutattr',
+        //       sortStrength: 50,
+        //       nodesFilter: (node: any) => node.uuid === '7d0740a7-5ee6-41a9-846b-d52890d690d5',
+        //     })
+        // },
+        {
+          type: 'radial',
+          center: [ 0, 0 ],
+          focusNode: '54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864_host_10.10.10.20',
+          unitRadius: 150,
+          maxIteration: 300,
+          linkDistance: 30,
+          preventOverlap: true,
+          nodeSize: 30,
+          sortBy: 'layoutattr',
+          sortStrength: 50,
+          nodesFilter: (node: any) => node.uuid === '54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864',
+        },
+        {
+          type: 'radial',
+          center: [ -800, 600],
+          focusNode: '070cb0b4-c415-4b6a-843b-efc51cff6b76_host_10.10.10.60',
+          unitRadius: 150,
+          maxIteration: 300,
+          linkDistance: 100,
+          preventOverlap: true,
+          nodeSize: 30,
+          sortBy: 'layoutattr',
+          sortStrength: 50,
+          nodesFilter: (node: any) => node.uuid === '070cb0b4-c415-4b6a-843b-efc51cff6b76',
+        },
+        {
+          type: 'radial',
+          center: [700, 500],
+          focusNode: '7d0740a7-5ee6-41a9-846b-d52890d690d5_host_10.10.10.111',
+          unitRadius: 150,
+          maxIteration: 300,
+          linkDistance: 100,
+          preventOverlap: true,
+          nodeSize: 30,
+          sortBy: 'layoutattr',
+          sortStrength: 50,
+          nodesFilter: (node: any) => node.uuid === '7d0740a7-5ee6-41a9-846b-d52890d690d5',
+        },
 
-      type: 'gForce',
-      gravity: 0.1,
-      edgeStrength: 10,
-      nodeStrength: 100,
+      ],
+  
 
+    
     },
+
+    // layout: {
+    //   // type: 'force',
+    //   // preventOverlap: true,
+    //   // linkDistance: 100,
+
+    //   // type: 'forceAtlas2',
+    //   // preventOverlap: true,
+    //   // kr: 30,
+
+    //   // type: 'radial',
+    //   // // focusNode: ['54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864_host_10.10.10.20', '070cb0b4-c415-4b6a-843b-efc51cff6b76_host_10.10.10.60', '7d0740a7-5ee6-41a9-846b-d52890d690d5_host_10.10.10.111'],
+    //   // focusNode: '54bcecd3-ea5f-497e-9ccb-3bb1aa9c0864_host_10.10.10.20',
+    //   // unitRadius: 100,
+    //   // maxIteration: 1000,
+    //   // linkDistance: 100,
+    //   // preventOverlap: true,
+    //   // nodeSpacing: 50,
+    //   // strictRadial: true,
+    //   // nodeSize: 30,
+    //   // sortBy: 'layoutattr',
+    //   // sortStrength: 50,
+    // },
     modes: {
-      default: ['drag-canvas', 'zoom-canvas', "click-select", "drag-node"],
+      default: ['drag-canvas', 'zoom-canvas', "click-select", "drag-node", 'drag-combo', 'collapse-expand-combo'],
     },
   });
   graph.node(function (node) {
@@ -200,7 +347,8 @@ function initGraph(data: any) {
     return false
   });
   graph.on('node:dragstart', (e) => {
-    graph.layout();
+    // graph.layout();
+    refreshDragedNodePosition(e);
   });
   graph.data(data);
   graph.render();
@@ -211,6 +359,12 @@ function initGraph(data: any) {
       document.getElementById("topo-container")!.clientHeight)
     graph.fitCenter()
   }
+}
+
+function refreshDragedNodePosition(e: any) {
+  const model = e.item.get('model');
+  model.fx = e.x;
+  model.fy = e.y;
 }
 
 function updateDrawer(node: any) {
@@ -235,6 +389,8 @@ function updateDrawer(node: any) {
   for (let i in node.model.tags) {
     tags.push(node.model.tags[i])
   };
+
+  console.log(node)
 }
 
 // 选择展示时间范围
@@ -424,5 +580,4 @@ const SizeAutoChange = (i: string, isChart?: boolean) => {
   box-sizing: border-box;
   cursor: pointer;
 }
-
 </style>
