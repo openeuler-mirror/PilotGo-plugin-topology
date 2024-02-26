6016583fbfa7bd934c2642c31da1c4f4f1cf0c61
zhaozhenfang <zhaozhenfang@kylinos.cn>
2024-02-26 16:55:10 +0800

    add basic config

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,22 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: () => import('@/App.vue')
-    //   // redirect: '/cluster'
-    // },
     {
-      path: '/node',
-      name: 'node',
-      component: () => import('../views/NodeView.vue')
+      path: '/',
+      redirect: '/topoList'
     },
     {
-      path: '/cluster',
-      name: 'cluster',
-      component: () => import('../views/ClusterView.vue')
-    }
+      path: '/topoList',
+      name: 'topoList',
+      component: () => import('../views/topoList.vue')
+    },
   ]
 })
 
